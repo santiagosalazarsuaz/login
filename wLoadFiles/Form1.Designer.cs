8106dba9524f2fc2a20951c79f4dcbd587689da1
santiagosalazarsuaz <113456937+santiagosalazarsuaz@users.noreply.github.com>
2022-11-14 22:37:41 -0500

    Add files via upload

---
+++ wLoadFiles/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace wLoadFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.cmdPorLineas = new System.Windows.Forms.Button();
+            this.gparchivo = new System.Windows.Forms.GroupBox();
+            this.dtGCSV = new System.Windows.Forms.DataGridView();
+            this.cmdCSV = new System.Windows.Forms.Button();
+            this.gparchivo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGCSV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(27, 21);
+            this.txtArchivo.Multiline = true;
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.Size = new System.Drawing.Size(344, 303);
+            this.txtArchivo.TabIndex = 0;
+            // 
+            // cmdPorLineas
+            // 
+            this.cmdPorLineas.Location = new System.Drawing.Point(84, 354);
+            this.cmdPorLineas.Name = "cmdPorLineas";
+            this.cmdPorLineas.Size = new System.Drawing.Size(134, 23);
+            this.cmdPorLineas.TabIndex = 1;
+            this.cmdPorLineas.Text = "Línea a Línea";
+            this.cmdPorLineas.UseVisualStyleBackColor = true;
+            this.cmdPorLineas.Click += new System.EventHandler(this.cmdPorLineas_Click);
+            // 
+            // gparchivo
+            // 
+            this.gparchivo.Controls.Add(this.dtGCSV);
+            this.gparchivo.Controls.Add(this.cmdCSV);
+            this.gparchivo.Controls.Add(this.txtArchivo);
+            this.gparchivo.Controls.Add(this.cmdPorLineas);
+            this.gparchivo.Location = new System.Drawing.Point(12, 2);
+            this.gparchivo.Name = "gparchivo";
+            this.gparchivo.Size = new System.Drawing.Size(1313, 428);
+            this.gparchivo.TabIndex = 2;
+            this.gparchivo.TabStop = false;
+            this.gparchivo.Text = "Cargar Archivo";
+            this.gparchivo.Enter += new System.EventHandler(this.gparchivo_Enter);
+            // 
+            // dtGCSV
+            // 
+            this.dtGCSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGCSV.Location = new System.Drawing.Point(440, 18);
+            this.dtGCSV.Name = "dtGCSV";
+            this.dtGCSV.RowTemplate.Height = 24;
+            this.dtGCSV.Size = new System.Drawing.Size(773, 305);
+            this.dtGCSV.TabIndex = 3;
+            // 
+            // cmdCSV
+            // 
+            this.cmdCSV.Location = new System.Drawing.Point(286, 354);
+            this.cmdCSV.Name = "cmdCSV";
+            this.cmdCSV.Size = new System.Drawing.Size(134, 23);
+            this.cmdCSV.TabIndex = 2;
+            this.cmdCSV.Text = "CSV";
+            this.cmdCSV.UseVisualStyleBackColor = true;
+            this.cmdCSV.Click += new System.EventHandler(this.cmdCSV_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1365, 486);
+            this.Controls.Add(this.gparchivo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gparchivo.ResumeLayout(false);
+            this.gparchivo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGCSV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.Button cmdPorLineas;
+        private System.Windows.Forms.GroupBox gparchivo;
+        private System.Windows.Forms.Button cmdCSV;
+        private System.Windows.Forms.DataGridView dtGCSV;
+    }
+}
+
---
+++ wLoadFiles/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+
+namespace wLoadFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Stream myStream;
+        int counter = 0;
+        string line;
+
+        private void cmdPorLineas_Click(object sender, EventArgs e)
+        {
+            // Este metodo permite cargar el archivo línea a línea,
+            // diferente al proyecto anterior donde primero se carga a una variable y luego
+            // se lleva a un control tipo texto.
+
+            XmlDocument Doc = new XmlDocument();
+            XmlElement raiz = Doc.CreateElement("libros");
+            Doc.AppendChild(raiz);
+            XmlElement libro = Doc.CreateElement("libro");
+            raiz.AppendChild(libro);
+            XmlElement titulo = Doc.CreateElement("titulo");
+            titulo.AppendChild(Doc.CreateTextNode("si"));
+            libro.AppendChild(titulo);
+
+            XmlElement precio = Doc.CreateElement("titulo");
+            titulo.AppendChild(Doc.CreateTextNode("si"));
+            libro.AppendChild(titulo);
+
+            Doc.Save("c:\\xml\\archivo.xml");
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            openFileDialog1.Filter = "Archivos (*.txt)|*.txt|(*xml)|*.xml|*All|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            System.IO.StreamReader file = new System.IO.StreamReader(openFileDialog1.FileName);
+                            while ((line = file.ReadLine()) != null)
+                            {
+                                txtArchivo.Text = txtArchivo.Text + line;
+                                counter++;
+                            }
+
+                            file.Close();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+          
+                }
+            }
+        }
+
+        private void gparchivo_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdCSV_Click(object sender, EventArgs e)
+        {
+            //Configurar datagridview - Número de columnas
+            DataGridViewTextBoxColumn col1 = new DataGridViewTextBoxColumn();
+            col1.HeaderText = "Columna1";
+            col1.Width = 200;
+            col1.ReadOnly = true;
+            dtGCSV.Columns.Add(col1);
+
+            DataGridViewTextBoxColumn col2 = new DataGridViewTextBoxColumn();
+            col2.HeaderText = "Columna2";
+            col2.Width = 200;
+            col2.ReadOnly = true;
+            dtGCSV.Columns.Add(col2);
+
+            DataGridViewTextBoxColumn col3 = new DataGridViewTextBoxColumn();
+            col3.HeaderText = "Columna3";
+            col3.Width = 200;
+            col3.ReadOnly = true;
+            dtGCSV.Columns.Add(col3);
+
+
+            char delimitador = ';';
+            string[] valores; 
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            openFileDialog1.Filter = "Archivos (*.csv)|*.csv";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            System.IO.StreamReader file = new System.IO.StreamReader(openFileDialog1.FileName);
+                            while ((line = file.ReadLine()) != null)
+                            {
+                                valores = line.Split(delimitador);
+                                
+                                dtGCSV.Rows.Add(valores.ToArray());
+                                //txtArchivo.Text = txtArchivo.Text + line;
+                                //counter++;
+                            }
+
+                            file.Close();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
